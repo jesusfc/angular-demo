d0f213e4481208fc6860a297fb9626c8f9e386da
Jesús Fdez. Caraballo <jfcaraballo@gmail.com>
2025-09-05 17:13:56 +0200

    add first commit

---
+++ src/app/components/my-components.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-components",
+  templateUrl: "./components/my-components.html",
+  standalone: true
+})
+export class MyComponentsComponent {
+  constructor() {
+  }
+}
