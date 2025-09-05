efd84a8231dd9783a1b6f8e1c2db9581010ac8a1
jfcaraballo <jfcaraballo@gmail.com>
2025-09-05 21:23:00 +0200

    update

--- src/app/components/my-components.ts
+++ src/app/components/my-components.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "my-components",
-  templateUrl: "./components/my-components.html",
+  templateUrl: "my-components.html",
   standalone: true
 })
 export class MyComponentsComponent {
